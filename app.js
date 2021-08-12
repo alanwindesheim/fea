024ad11854077e06e6e66b7ecc00a679d6d1d5c0
alanwindesheim <55313885+alanwindesheim@users.noreply.github.com>
2021-08-12 16:36:34 +0200

    ddd

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv/config')
-app.use(express.static(path.join(__dirname, 'build')));
+const mongoose = require("mongoose");
+require("dotenv/config");
+app.use(express.static(path.join(__dirname, "build")));
 
 //convert data to json
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 mongoose.pluralize(null);
 
 //import routers
-const liftRoute = require('./routes/liften');
-const liftHistoryRoute = require('./routes/liftHistory');
+const liftRoute = require("./routes/liften");
+const liftHistoryRoute = require("./routes/liftHistory");
 
 //use routers
-app.use('/liften', liftRoute);
-app.use('/liftenhistory', liftHistoryRoute);
+app.use("/liften", liftRoute);
+app.use("/liftenhistory", liftHistoryRoute);
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 connectDb();
 
 async function connectDb() {
-  try{
-    await mongoose.connect(
-      process.env.DB_CONNECTION,
-      {useNewUrlParser: true, useUnifiedTopology: true }
-  );
-  }catch(error){
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
     console.log(error);
   }
-  
 }
 
-
-
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
